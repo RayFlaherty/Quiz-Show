1e4cc7730cad7bcc41c80019cdcba1e3d76b6450
RayFlaherty <raymondf.flaherty@gmail.com>
2021-11-06 01:37:25 -0500

    clean up and clock manipulation

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,21 @@
-var buttonEl = document.querySelector("#start");
+
 var question1El = document.querySelector("#question1");
 
-
-
-var startgame = function timer() {
-  var timeleft = 60;
+const buttonEl=document.querySelector("#start");
+    buttonEl.onclick = () =>{
+      var timeleft = 60;
   var downloadTimer = setInterval(function(){
     if(timeleft <= 0){
       clearInterval(downloadTimer);
       document.getElementById("countdown").innerHTML = "TIMES UP!";
     } else {
-      document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+      document.getElementById("countdown").innerHTML = ":" + timeleft;
     }
     timeleft -= 1;
   }, 1000);
- 
+      var question1El
 
-buttonEl.addEventListener("click",startgame);
+    }
+
+    
+
